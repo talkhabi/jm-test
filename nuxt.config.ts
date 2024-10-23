13bd12068d68bcc9cdd1ee765aafafea6cec47f2
m.talkhabi <mt382861@gmail.com>
2024-10-23 22:55:16 +0330

    feat: load main font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  app: {
+    head: {
+      link: [
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=swap' }
+      ]
+    }
+  },
 })
